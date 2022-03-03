7647df143d2e6c9e8e4c98f90384ed1fd4f65a17
adrianojiu <adrianojiu@outlook.com>
2022-03-03 15:40:45 -0300

    add sqlite

---
+++ SQLite/SWInventoryToCSV.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module PSSQLite
+
+$dateSW = get-date -Format dd-MM-yyyy-hh
+$SpiceDatabase = "C:\Program Files (x86)\Spiceworks\db\spiceworks_prod.db"
+$SpiceWInventory = Invoke-SqliteQuery -DataSource $SpiceDatabase -Query "SELECT id,name,operating_system,version,manufacturer,model,serial_number,memory,number_of_processors,device_type FROM devices"
+
+$SpiceWInventory | Export-Csv -Path C:\Inventory-Share\SWinventory_$dateSW.csv -NoTypeInformation
